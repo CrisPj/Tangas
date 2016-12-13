424d9328340515e1a498f37286d587ce94272dcf
Cristian Perez <creshandkesh@gmail.com>
2016-12-13 14:04:09 -0600

    check

--- app/src/main/java/tiendita/com/tienda/api/WoocommerceAPI.java
+++ app/src/main/java/tiendita/com/tienda/api/WoocommerceAPI.java
@@ -1,18 +1,14 @@
 package tiendita.com.tienda.api;
 
-import java.util.HashMap;
 import java.util.Map;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
-import retrofit2.http.FieldMap;
-import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.Headers;
 import retrofit2.http.POST;
 import retrofit2.http.QueryMap;
 import tiendita.com.tienda.pojo.Coupons;
-import tiendita.com.tienda.pojo.Product;
 
 
 public interface WoocommerceAPI {
